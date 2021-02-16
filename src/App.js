2fc3790ab94956cbb125e77ff9f9e0ecacdd8ad5
willernst95 <ernstwill4224@gmail.com>
2021-02-16 14:48:56 -0600

    Update to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import './App.css';
 import React from "react"
+import { Switch, Route, BrowserRouter as Router, Redirect, NavLink} from "react-router-dom";
+import LoginPage from "./components/Login/LoginPage";
+import SearchPage from "./components/Search/SearchPage";
+import FavoritesPage from "./components/Favorites/FavoritesPage";
 
 function App() {
   return (
+    <Router>
   <>
-    <nav></nav>
-    <main></main>
+    <nav>
+      {/* Links for login/ search/ favorites */}
+      <NavLink to="/login" activeClassName ="active-link">
+        Login
+        </NavLink>
+      <NavLink to="/search" activeClassName ="active-link">Search</NavLink>
+      <NavLink to="/favorites" activeClassName ="active-link">Favorites</NavLink>
+    </nav>
+    <main>
+      <Switch>
+        <Route path="/login" component ={LoginPage} />
+        <Route path="/search" component ={SearchPage} />
+        <Route path="/favorites" component ={FavoritesPage} />
+        <Route path="*" >
+          <Redirect to ="/search" />
+          </Route>
+      </Switch>
+    </main>
     <footer>This is a footer</footer>
   </>
+  </Router>
   );
 }
 
--- src/Components/Search/SearchPage.js
+++ src/Components/Search/SearchPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const SearchPage = (props) => {
+    // Inputs / Button / Function (here or elsewhere to trigger the fetch call)
     return <h1>SearchPage Works</h1>
 }
 
